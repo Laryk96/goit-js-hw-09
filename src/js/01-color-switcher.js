0370248bada4a481064b1145418fee1fae875802
Lary <110429721+Laryk96@users.noreply.github.com>
2022-12-05 14:24:37 +0200

    add

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,63 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStart.addEventListener('click', () => {
+  colorChange.startChangeColor();
+});
+refs.btnStop.addEventListener('click', () => {
+  colorChange.stopChangeColor();
+});
+
+const colorChange = {
+  startChangeColor() {
+    this.intercalId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+  },
+
+  stopChangeColor() {
+    clearInterval(this.intercalId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+  },
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Вариант 2
+
+// refs.btnStart.addEventListener('click', onClickStart);
+// refs.btnStop.addEventListener('click', onClickStop);
+
+// function onClickStart() {
+//   changeColor();
+//   refs.btnStart.disabled = true;
+//   refs.btnStop.disabled = false;
+// }
+
+// function onClickStop() {
+//   refs.btnStart.disabled = false;
+//   refs.btnStop.disabled = true;
+// }
+
+// function changeColor() {
+//   const intercalId = setInterval(() => {
+//     if (refs.btnStop.disabled) {
+//       clearInterval(intercalId);
+//       return;
+//     }
+//     refs.body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
